daf6c5b24686abc89fc83be6950e4967c7c5a6d2
Martin Chaperot <martin.chapino@gmail.com>
2025-06-03 17:56:11 -0400

    configurable hci (it can change for some reason?)

--- level.sh
+++ level.sh
@@ -2,11 +2,12 @@
 
 # Check `bluetoothctl gatt.list-attributes` to find paths for `Battery Level`
 
+HCI=hci0
 MAC_ADDR=F4_70_72_46_2B_96
 L_KB_PATH=service0010/char0011
 R_KB_PATH=service0015/char0016
 
-L_KB_HEX=$(gdbus call --system --dest org.bluez --object-path /org/bluez/hci2/dev_$MAC_ADDR/$L_KB_PATH --method org.bluez.GattCharacteristic1.ReadValue "{}"  | sed -E 's/.*0x([0-9a-fA-F]+).*/\1/');
-R_KB_HEX=$(gdbus call --system --dest org.bluez --object-path /org/bluez/hci2/dev_$MAC_ADDR/$R_KB_PATH --method org.bluez.GattCharacteristic1.ReadValue "{}"  | sed -E 's/.*0x([0-9a-fA-F]+).*/\1/');
+L_KB_HEX=$(gdbus call --system --dest org.bluez --object-path /org/bluez/$HCI/dev_$MAC_ADDR/$L_KB_PATH --method org.bluez.GattCharacteristic1.ReadValue "{}"  | sed -E 's/.*0x([0-9a-fA-F]+).*/\1/');
+R_KB_HEX=$(gdbus call --system --dest org.bluez --object-path /org/bluez/$HCI/dev_$MAC_ADDR/$R_KB_PATH --method org.bluez.GattCharacteristic1.ReadValue "{}"  | sed -E 's/.*0x([0-9a-fA-F]+).*/\1/');
 
 echo "Left: $((16#$L_KB_HEX))%; Right: $((16#$R_KB_HEX))%"
